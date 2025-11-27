5017cec688cc1af5255369891fec7ef16ceecff2
Mazzare11o <godblessmabel3@gmail.com>
2025-11-27 10:54:29 +0100

    My journey

--- titans_work.py
+++ titans_work.py
@@ -1,3 +1,11 @@
+import pandas as pd
+
+# Read from CSV & Excel Files
+df_for_csv_original = pd.read_csv(r"C:\Users\godbl\Downloads\student_performance_data.csv")
+
+# Copied the Original data into variable
+df_csv_copied = df_for_csv_original.copy()
+
 # Read from CSV & Excel Files
 df_for_csv_original = pd.read_csv(r"C:\Users\godbl\Downloads\student_performance_data.csv")
 
